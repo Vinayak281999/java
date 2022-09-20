9f5b72ea5197309a88a41d109a90a39b79196c95
Vinayak281999 <vinayak.xworkz@gmail.com>
2022-09-20 13:03:22 +0530

    ok

---
+++ ShowRoom.java
@@ -0,0 +1,26 @@
+class ShowRoom{
+
+static void getcars(String[] cars){
+System.out.println("inside getcars()");
+System.out.println("Size of Cars"+cars.length);
+System.out.println("List of Cars");
+  for(int index=0;index<cars.length;index++){
+ System.out.println(cars [index]); 
+  }
+  System.out.println("end of cars"); 
+  
+
+}
+System.out.println(...............................); 
+static void getbike(String[] Bike){
+System.out.println("inside getBike()");
+System.out.println("Size of Bike"+Bike.length);
+System.out.println("List of bikeskes");
+for(int index=0;index<Bike.length;index++){
+System.out.println(Bike[index]);
+}
+System.out.println("end of Bike");
+}
+
+
+}
